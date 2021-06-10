45a84e2362ad03e066f65e3b36b84674f54a3448
João Hugo Leda <joaohugoleda@gmail.com>
2021-06-10 00:27:04 -0300

    componentes

--- calculadora-react/src/App.js
+++ calculadora-react/src/App.js
@@ -1,11 +1,35 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useState } from 'react';
 
 function App() {
+
+  const [valorTela, setValorTela] = useState('')
+  const [resultado, setResultado] = useState(0)
+  const [acumulador, setAcumulador] = useState(0)
+  const [operacao, setOperacao] = useState(false)
+
+  //Componentes
+  const Tela = (valor, res) => {
+    
+    return (
+      <div className="tela">
+        <span className="operacao">{valor}</span>
+        <span className="res">{res}</span>
+      </div>
+    )
+  }
+
+  const Btn = (lbl, onClick) => {
+    return (
+      <button className="btn" onClick={onClick}>{lbl}</button>
+    )
+  }
+
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      {Tela(0,50)}      
+    </>
   );
 }
 
