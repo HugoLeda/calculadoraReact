4f991a67c85ac483a22246c7bdfab87ccac10f2f
João Hugo Leda <joaohugoleda@gmail.com>
2021-06-10 23:29:49 -0300

    funcoes

--- calculadora-react/src/App.js
+++ calculadora-react/src/App.js
@@ -7,15 +7,15 @@ function App() {
   const [valorTela, setValorTela] = useState('')
   const [resultado, setResultado] = useState(0)
   const [acumulador, setAcumulador] = useState(0)
-  const [operacao, setOperacao] = useState(false)
+  const [operado, setOperado] = useState(false)
 
   //Componentes
   const Tela = (valor, res) => {
     
     return (
       <div className="tela">
-        <span className="operacao">{valor}</span>
         <span className="res">{res}</span>
+        <span className="operacao">{valor}</span>        
       </div>
     )
   }
@@ -26,9 +26,67 @@ function App() {
     )
   }
 
+  //Funcoes
+  const addDigitoTela = (d) => {
+    if ((d == '+' || d == '-' || d == '/' || d == '*') && (operado)) {
+      console.log("+-*/")
+      setOperado(false)
+      setValorTela(resultado + d)
+      return
+    } else if (operado)     {
+      setValorTela(d)
+      setOperado(false)
+    }
+
+    const valorDigitadoTela = valorTela + d
+    setValorTela(valorDigitadoTela)
+  }  
+
+  const limparMemoria = () => {
+    setOperado(false)
+    setValorTela('')
+    setResultado(0)
+    setAcumulador(0)
+  }
+
+  const operacao = (oper) => {
+    if (oper == 'bs') {
+      let vTela = valorTela
+      vTela = vTela.substring(0, vTela.length - 1)
+      setValorTela(vTela)
+      setOperado(false)
+      return
+    }
+
+    try {
+      const r = eval(valorTela)
+      setAcumulador(r)
+      setResultado(r)
+      setOperado(true)
+    } catch {
+      setResultado('ERRO')
+    }
+  }
+
   return (
     <>
-      {Tela(0,50)}      
+      <div className="container">
+        <h3>Calculadora Simples</h3>
+        {Tela(valorTela, resultado)}
+        <div className="teclado">
+          {Btn('AC', limparMemoria)}
+          {Btn('(', () => addDigitoTela('('))}
+          {Btn(')', () => addDigitoTela(')'))}
+          {Btn('/', () => addDigitoTela('/'))}
+          {Btn('7', () => addDigitoTela('7'))}
+          {Btn('8', () => addDigitoTela('8'))}
+          {Btn('9', () => addDigitoTela('9'))}
+          {Btn('*', () => addDigitoTela('*'))}
+          {Btn('4', () => addDigitoTela('4'))}
+          {Btn('5', () => addDigitoTela('5'))}
+          {Btn('6', () => addDigitoTela('6'))}
+        </div>
+      </div>     
     </>
   );
 }
